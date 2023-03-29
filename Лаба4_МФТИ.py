3cd0f52b56c6fc8bc6f47a3a58242616cd85a639
TsetsenDoraev <129021813+TsetsenDoraev@users.noreply.github.com>
2023-03-29 08:39:25 +0300

    Update Лаба4_МФТИ.py

--- Лаба4_МФТИ.py
+++ Лаба4_МФТИ.py
@@ -1,13 +1,14 @@
-git clone https://github.com/Ivanov/infa_2020_doraev
-Cloning into 'infa_2020_doraev'...
-remote: Counting objects: 3, done.
-remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
-Unpacking objects: 100% (3/3), done.
-ls
-infa_2020_doraev
-cd infa_2020_doraev
-ls
-README.md
-
-git config user.name "Doraev Tsetsen"
-git config user.email ivanov.doraevtsetsen@gmail.com
+# git clone https://github.com/Ivanov/infa_2020_doraev
+# Cloning into 'infa_2020_doraev'...
+# remote: Counting objects: 3, done.
+# remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+# Unpacking objects: 100% (3/3), done.
+# ls
+# infa_2020_doraev
+# cd infa_2020_doraev
+# ls
+# README.md
+#
+# git config user.name "Doraev Tsetsen"
+# git config user.email ivanov.doraevtsetsen@gmail.com
+print('Hello world epta!')
